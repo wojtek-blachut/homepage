c4deef41a0751be9d93a88b14087e8b0af23e274
Wojtek B <wojtek.study@gmail.com>
2020-05-10 22:37:54 +0200

    add js inner lines to index.html + modification in styling

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const surname = "Blachut";
 console.log(
   `Siemka z tej strony ${name} ${surname}. Życzę miłego przeglądania strony. Pozdrawiam!`
 );
+
+let footer = document.querySelector('.inner-js');
+
+footer.innerHTML = 'Tekst dodany przez JS\'a';
