fceebf5bc2337c48ca525e175e615b89b4b392b4
Wojtek B <wojtek.study@gmail.com>
2020-05-23 18:19:35 +0200

    Centralized navigation menu

--- main.js
+++ main.js
@@ -5,6 +5,38 @@ console.log(
   `Siemka z tej strony ${name} ${surname}. Życzę miłego przeglądania strony. Pozdrawiam!`
 );
 
-let footer = document.querySelector('.inner-js');
+let footer = document.querySelector(".inner-js");
 
-footer.innerHTML = 'Tekst dodany przez JS\'a';
+footer.innerHTML = "Tekst dodany przez JS'a";
+
+function showWelcomeMessage(name, surname) {
+  console.log(`Hej, jestem ${name} ${surname}, witam na mojej stronie domowej`);
+}
+
+const showWelcomeMessageArrow = (name, age) => {
+  console.log(`Hej, jestem ${name} ${age}, witam na mojej stronie domowej`);
+};
+
+showWelcomeMessageArrowT = (name, age) => {
+  console.log(
+    `Hej, jestem ${name} ${age}, witam na mojej stronie domowej TESTSSS`
+  );
+};
+
+showWelcomeMessageArrowT("Wojtek", "32");
+
+welcomeFormula = {
+  name: "Wacek",
+  age: 32,
+  sayHello: (name) => {
+    console.log(`${name} saying hello from the object`)
+  },
+};
+
+welcomeFormula.sayHello(welcomeFormula.name);
+
+showMeProperty = (myProperty) =>{
+  console.log(welcomeFormula[myProperty])
+}
+
+showMeProperty('age')
